825908d0e6874c086901e8a2da3ae1fa2945b373
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-09-12 22:59:12 +0530

    clear errors

--- src/Pages/HomePage/Skills.jsx
+++ src/Pages/HomePage/Skills.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import data from "../../DataFile/index.json";
 
 function Skills() {
   return (
@@ -10,32 +9,6 @@ function Skills() {
           <p className="SubTitle">Skills</p>
         </div>
         <div className="SkillsSectionContainer">
-          {/* {data?.skills?.map((item, index) => (
-            <div key={index} className="SkillsSectionCard">
-              <div className="SkillsSectionImg">
-                <img src={item.src} alt="ProductChain" />
-              </div>
-              <div className="SkillsSectionCardContent">
-                <h3 className="SkillsSectionTitle">{item.title}</h3>
-                <div className="item-content">
-                  <p className="SkillsSectionDescription">{item.item1}</p>
-                  <img className="item-img" src={item.img1} alt="" />
-                </div>
-                <div className="item-content">
-                  <p className="SkillsSectionDescription">{item.item2}</p>
-                  <img className="item-img" src={item.img2} alt="" />
-                </div>
-                <div className="item-content">
-                  <p className="SkillsSectionDescription">{item.item3}</p>
-                  <img className="item-img" src={item.img3} alt="" />
-                </div>
-                <div className="item-content">
-                  <p className="SkillsSectionDescription">{item.item4}</p>
-                  <img className="item-img" src={item.img4} alt="" />
-                </div>
-              </div>
-            </div>
-          ))} */}
           <div className="skill-box">
             <img src="./img/JavaScript-logo.png" alt="" />
             <p className="skill-name">Java Script</p>
